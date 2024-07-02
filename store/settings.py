58d3964cc5611134b4b0c9fd73f683c1fd2c3464
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-07-02 21:51:23 +0300

    build: add products app

--- store/settings.py
+++ store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l#!+7%+gi62*^37)rsg)#f^h^cnx8hid_8$8ai$iy8+f006=57
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
