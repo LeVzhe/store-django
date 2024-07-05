93191ac7c260fcb62c394e71553ebe63d9f57cb7
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-07-05 23:11:56 +0300

    fix: set correct admin.site views from Meta

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product, ProductCategory
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'quantity', 'image', 'category')
+    list_display_links = ('name', 'category')
+
+admin.site.register(Product, ProductAdmin)
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    list_display_links = ('name', 'description')
+
+admin.site.register(ProductCategory, ProductCategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-    description = models.TextField(blank=True, null=True)
+    name = models.CharField(max_length=128, unique=True, verbose_name='Название')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
     
 class Product(models.Model):
-    name = models.CharField(max_length=256)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products_images')
-    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Цена')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    image = models.ImageField(upload_to='products_images', verbose_name='Картинка')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE, verbose_name='Категория')
+    
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
