df0421593ff286f88b7c78a11d52ff8d9b55ce10
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-07-05 23:54:52 +0300

    feat: fill the model; some fixes

--- products/models.py
+++ products/models.py
@@ -8,10 +8,14 @@ class ProductCategory(models.Model):
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
     
+    def __str__(self):
+        return self.name
+    
+    
 class Product(models.Model):
     name = models.CharField(max_length=256, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
-    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Цена')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
     image = models.ImageField(upload_to='products_images', verbose_name='Картинка')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE, verbose_name='Категория')
@@ -19,3 +23,6 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
+        
+    def __str__(self):
+        return f'Продукт: {self.name} | категория: {self.category.name}'
