8f53087b97bd35ed25f9b177f4d8f4959ce12425
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-07-07 23:37:57 +0300

    feat: add profile logic

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,9 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from dataclasses import field
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserCreationForm,
+    UserChangeForm,
+)
 from django import forms
 
 from .models import User
@@ -87,3 +92,38 @@ class UserRegistrationForm(UserCreationForm):
             "password1",
             "password2",
         )
+
+
+class UserProfileForm(UserChangeForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control py-4",
+            }
+        )
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control py-4",
+            }
+        )
+    )
+    image = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                "class": "custom-file-label",
+            }
+        ),
+        required=False,
+    )
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control py-4", "readonly": True})
+    )
+    email = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control py-4", "readonly": True})
+    )
+
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "image", "username", "email"]
