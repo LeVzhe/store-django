a371c62f25eb3b0ff215a4be0552849e4dfb6f91
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-07-07 22:40:58 +0300

    feat: create registration logic

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib import auth
 from django.urls import reverse
 
 # from .models import User
-from .forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -15,12 +15,20 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
+                return HttpResponseRedirect(reverse("index"))
     else:
         form = UserLoginForm()
-    context = {"form": UserLoginForm()}
+    context = {"form": form}
     return render(request, "users/login.html", context)
 
 
 def registration(request):
-    return render(request, "users/registration.html")
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("users:login"))
+    else:
+        form = UserRegistrationForm()
+    context = {"form": form}
+    return render(request, "users/registration.html", context)
