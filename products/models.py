331a146567a22d6c1ce1d6504d94ec238653b060
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-07-19 23:50:28 +0300

    feat: add more tests

--- products/models.py
+++ products/models.py
@@ -32,12 +32,14 @@ class Product(models.Model):
     def __str__(self):
         return f"Продукт: {self.name} | категория: {self.category.name}"
 
+
 class BasketQuerySet(models.QuerySet):
     def total_sum(self):
         return sum(basket.sum() for basket in self)
-    
+
     def total_quantity(self):
         return sum(basket.quantity for basket in self)
+
 
 class Basket(models.Model):
     user = models.ForeignKey(
@@ -50,7 +52,7 @@ class Basket(models.Model):
     created_timestamp = models.DateTimeField(
         auto_now_add=True, verbose_name="Время создания"
     )
-    
+
     objects = BasketQuerySet.as_manager()
 
     class Meta:
--- products/tests.py
+++ products/tests.py
@@ -1,5 +1,9 @@
+from http import HTTPStatus
+
 from django.test import TestCase
 from django.urls import reverse
+
+from products.models import Product, ProductCategory
 
 
 class IndexViewTestCase(TestCase):
@@ -8,6 +12,39 @@ class IndexViewTestCase(TestCase):
         path = reverse("index")
         response = self.client.get(path)
 
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.context_data["title"], "Store")
         self.assertTemplateUsed(response, "products/index.html")
+
+
+class ProductsListViewTestCase(TestCase):
+
+    fixtures = ["categories.json", "goods.json"]
+
+    def setUp(self):
+        self.products = Product.objects.all()
+
+    def _common_tests(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data["title"], "Store  Товары")
+        self.assertTemplateUsed(response, "products/products.html")
+
+    def test_list(self):
+        path = reverse("products:index")
+        response = self.client.get(path)
+
+        self._common_tests(response)
+        self.assertEqual(
+            list(response.context_data["object_list"]), list(self.products[:3])
+        )
+
+    def test_list_with_category(self):
+        category = ProductCategory.objects.first()
+        path = reverse("products:category", kwargs={"category_id": category.id})
+        response = self.client.get(path)
+
+        self._common_tests(response)
+        self.assertEqual(
+            list(response.context_data["object_list"]),
+            list(self.products.filter(category_id=category.id)),
+        )
