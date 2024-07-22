e153a59644407b7dce539699b524db90eefce2c7
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-07-22 23:05:32 +0300

    feat: create view, urls, templat logic

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+# from django.views.generic.edit import CreateView
+
+# from django.urls import reverse, reverse_lazy
+
+
+class OrderCreateView(TemplateView):
+    template_name = "orders/order-create.html"
+    # model = User
+    # form_class = UserRegistrationForm
+    # success_url = reverse_lazy("users:login")
+    # success_message = "Поздравляем, вы успешно зарегистрировались!"
+    # title = "Store - Регистрация"
