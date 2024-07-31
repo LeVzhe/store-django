8070903024d517d5b46112f0c3b02f37085fdf44
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-07-31 21:42:46 +0300

    feat: add pagination django Rest and category.name true presentation

--- products/serializers.py
+++ products/serializers.py
@@ -4,6 +4,8 @@ from products.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(read_only=True, slug_field="name")
+
     class Meta:
         model = Product
         fields = (
